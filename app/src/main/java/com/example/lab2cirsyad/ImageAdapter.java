6403d79bed3d979b73241c15bc834c8201f478fb
amirulirsyad <2020489528@student.uitm.edu.my>
2021-11-18 23:06:14 +0800

    Lab2c irsyad

---
+++ app/src/main/java/com/example/lab2cirsyad/ImageAdapter.java
@@ -0,0 +1,33 @@
+package com.example.lab2cirsyad;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public Integer[] thumbImages = {
+            R.drawable.bmw,R.drawable.neutron,R.drawable.ohio,R.drawable.skoda,
+            R.drawable.superman,R.drawable.thunder
+    };
+    public ImageAdapter(Context c){ mContext = c; }
+    public int getCount(){return thumbImages.length; }
+    public Object getItem(int position){return null;}
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        ImageView imageView = new ImageView(mContext);
+        imageView.setLayoutParams(new GridView.LayoutParams(250,250));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setPadding(8 ,8 ,8 ,8);
+        imageView.setImageResource(thumbImages[position]);
+        return imageView;
+    }
+}
